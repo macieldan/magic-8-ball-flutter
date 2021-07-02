7aab39b1d55f9d2b8c3d9d75b964c9d237ca009a
Daniel Maciel <daniel_maciel.04@hotmail.com>
2021-07-02 18:58:39 -0300

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,51 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.amber,
+        appBar: AppBar(
+          title: Text('Pergunte qualquer coisa'),
+          backgroundColor: Colors.green,
+        ),
+        body: Ball(),
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ball = 0;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Center(
+        child: Container(
+          child: FlatButton(
+            onPressed: () {
+              setState(() {
+                print('cliquei');
+                ball = Random().nextInt(5) + 1;
+              });
+            },
+            child: Image.asset('images/ball$ball.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
